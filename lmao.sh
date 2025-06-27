b95bdf00d9d8bb2c497ddc0a3201bb9d1a787e29
rye <lepyrogang@proton.me>
2025-06-27 07:02:11 +0330

    feat: Screenshot from 2025-06-27 06-47-39.png

--- lmao.sh
+++ lmao.sh
@@ -5,3 +5,7 @@ cp "/home/rye/Pictures/Screenshots/${fname}" .
 git add .
 git commit -m "feat: ${fname}"
 git push
+
+sleep 5
+
+echo "https://raw.githubusercontent.com/northlightz/img-dump/refs/heads/main/${fname}"
