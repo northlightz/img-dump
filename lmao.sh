9f25934ed78383ed758885da2d449aa98b83fb5b
rye <lepyrogang@proton.me>
2025-06-27 07:15:19 +0330

    feat: Screenshot from 2025-06-27 07-03-09.png

--- lmao.sh
+++ lmao.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/bash
+oldestfile=$(ls -1t '/home/rye/Pictures/Screenshots/' | tail -n 1)
+filecount=$(ls -1 | wc -l) - 1
+
+if [$filecount <= 10]; then
+	rm -f $oldestfile
+fi
+
 fname=$(ls -1t '/home/rye/Pictures/Screenshots/' | head -n 1)
 
 cp "/home/rye/Pictures/Screenshots/${fname}" .
@@ -6,6 +13,6 @@ git add .
 git commit -m "feat: ${fname}"
 git push
 
-sleep 5
+sleep 3
 urlfname=$(printf "${fname}" | jq -sRr @uri)
 echo "https://raw.githubusercontent.com/northlightz/img-dump/refs/heads/main/${urlfname}" | wl-copy
