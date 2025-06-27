783d3d9ea2732ae1c9a8f58ce81c1179d7489c98
rye <lepyrogang@proton.me>
2025-06-27 06:54:21 +0330

    feat: Screenshot from 2025-06-27 06-47-39.png

--- lmao.sh
+++ lmao.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
-fname=$(ls -1t '~/Pictures/Screenshots/' | head -n 1)
+fname=$(ls -1t '/home/rye/Pictures/Screenshots/' | head -n 1)
 
-cp ~/Pictures/Screenshots/${fname} .
+cp '/home/rye/Pictures/Screenshots/${fname}' .
 git add .
 git commit -m "feat: ${fname}"
 git push
